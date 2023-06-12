dcb1d5a511cd63dbae2007218c2a9aecdd0b9dd6
David Rowe <36871115+dp-rho@users.noreply.github.com>
2023-06-12 11:48:02 -0400

    Update execute_commands.cpp
    remove call_device() extern

--- inst/compile/buildDyn/src/execute_commands.cpp
+++ inst/compile/buildDyn/src/execute_commands.cpp
@@ -1,7 +1,5 @@
 #include "commands.h"
 
-/* Function which calls the machine generated kernel code */
-extern void call_device();
 
 /* Shell function used to call .cu compiled code through Rcpp interface */
 // [[Rcpp::export]]
